33e4623218ce89cd5ddf8a711a50a8461f415647
Thea <tmush2014@agruni.edu.ge>
2017-11-25 23:02:24 +0400

    Video Speed

--- video_speed/main.js
+++ video_speed/main.js
@@ -1,4 +1,4 @@
-const speed = document.querySelector('.speed');
+  const speed = document.querySelector('.speed');
   const bar = speed.querySelector('.speed-bar');
   const video = document.querySelector('.flex');
 
